f84d5ca75e3d17e605602fc76372911bc2c9a43d
jinified <jinified@gmail.com>
2018-11-28 09:12:37 +0800

    Added logging

--- src/index.js
+++ src/index.js
@@ -12,8 +12,10 @@ const get = async (featureId) => {
   const key = `${SERVICE_NAME}-${featureId}`;
   try {
     const val = await cache.get(key);
+    console.log("Cache: ", val)
     if (val === null || val === undefined) {
       const received = await featureStore.getRecord(featureId);
+      console.log("DB: ", received)
       // Stores result in cache
       cache.put(key, received);
       return received;
