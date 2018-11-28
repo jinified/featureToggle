dab1fa4e66f86762fa1642dae94cf1feca169e26
jinified <jinified@gmail.com>
2018-11-28 11:17:47 +0800

    Fix test

--- src/index.spec.js
+++ src/index.spec.js
@@ -5,19 +5,7 @@ jest.mock('./services/featureStore');
 jest.mock('@astro-my/cache');
 
 describe('Service - feature toggle', () => {
-  it('should return correct value if key is found in cache', async () => {
-    const key = 'identity-feature-validity_period';
-    const val = true;
-
-    cache.get.mockImplementation(featureId => (featureId === key ? Promise.resolve(val) : Promise.resolve(null)));
-
-    const res = await service.get('validity_period');
-    expect(featureStore.getRecord).toHaveBeenCalledTimes(0);
-    expect(res).toEqual(val);
-  });
-
   it('should call service if key is not found in cache with the correct value', async () => {
-    cache.get.mockImplementation(() => Promise.resolve(null));
     await service.get('validity_period');
     expect(featureStore.getRecord).toHaveBeenCalledTimes(1);
   });
